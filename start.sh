c9c7e4d95a99141087a39a31c7258f2a225a171a
Deekshith SH <shdeekshith@gmail.com>
2024-06-07 01:10:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
-git clone https://github.com/EverythingSuckz/TG-FileStreamBot.git .
+git clone https://github.com/EverythingSuckz/TG-FileStreamBot.git
+cd TG-FileStreamBot
 go build ./cmd/fsb/
+mv fsb ../fsb
 chmod +x fsb
